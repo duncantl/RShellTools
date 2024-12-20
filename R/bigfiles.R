0e257eb177e3c78accb56870fbd7a1cedf59b2f4
Duncan Temple Lang <duncan@r-project.org>
2024-12-19 20:22:29 -0800

    make ext included by default and rearrange the code to make it simpler

--- R/bigfiles.R
+++ R/bigfiles.R
@@ -1,7 +1,12 @@
 bigfiles =
-function(dir = ".", recursive = FALSE)
+function(dir = ".", recursive = FALSE, ext = TRUE)
 {
-  i = file.info(list.files(dir, full.names = TRUE, recursive = recursive))    
-  i[order(i$size, decreasing = TRUE), c("size", "ctime"), drop = FALSE] 
+    i = file.info(list.files(dir, full.names = TRUE, recursive = recursive))
+
+    i = i[, c("size", "ctime")]
+    if(ext) 
+        i$ext = tools::file_ext(rownames(i))
+        
+    i[order(i$size, decreasing = TRUE), , drop = FALSE] 
 }
 
